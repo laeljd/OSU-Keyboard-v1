a82559ef8741fc17fc0265c9f0df00751a3ffb28
Lael Jader <lael.jader@gmail.com>
2025-09-26 18:15:29 -0300

    initial PlatformIO project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+// ...inclua bibliotecas HID USB quando necessário...
+
+void setup()
+{
+    // ...existing code...
+    // Inicialização do teclado HID USB
+}
+
+void loop()
+{
+    // ...existing code...
+    // Leitura dos botões e envio de eventos HID
+}
